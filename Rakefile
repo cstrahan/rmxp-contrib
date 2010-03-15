d7c1c9f337a5048a02d5ecb0140e59cc8ca56a87
Charles Strahan <charles.c.strahan@gmail.com>
2010-03-15 02:53:47 -0500

    Add yaml_dumper.rb

--- Rakefile
+++
@@ -1,4 +0,0 @@
-# 
-# TODO: Add a task to 'build' the scripts - rolling them into one script.
-#       Make sure to sort the scripts by their dependancies, then remove all calls to Kernal#require
-# 
---
+++ lib/yaml_dumper.rb
@@ -0,0 +1,23 @@
+require 'yaml'
+
+# This is a utility class for ripping content from *.rxdata files.
+# It's designed to dump large data files without crashing the game engine.
+class YAMLDumper
+  def self.dump(obj, file)
+    # Keep the game from crashing due to "hanging script"
+    Graphics.update
+    count = 0
+
+    # Dumping one item at a time helps prevent the game from timing out
+    Array(obj).each do |e|
+      YAML.dump(e, file)
+
+      # Keep the game from crashing due to "hanging script"
+      count += 1
+      if count % 2 == 0
+        Graphics.update
+        count = 0
+      end
+    end
+  end
+end
